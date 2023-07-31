134f813c35158e21de7c49a5bb440be72834b6b1
monsaan <dr.myatmonsan@gmail.com>
2023-07-31 15:39:48 +0630

    hello.dart

---
+++ hello.dart
@@ -0,0 +1,13 @@
+void main()  
+{
+String myname = "Myat Mon San";
+int myage = 30;
+bool isFemale =true;
+double myMark =7.5;
+print(myname);
+print(myage);
+print(isFemale);
+print(myMark);
+String a ="1234";
+int mySurName = int.parse(a);
+print(mySurName.runtimeType);}
